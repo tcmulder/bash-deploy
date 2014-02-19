afda66a63f77fbe89d87d4f349fca67bd37e76f7
tcmulder <tcmulder@gmail.com>
2014-02-18 21:49:00 -0700

    Add put code

--- old/_put_code.sh
+++ old/_put_code.sh
@@ -6,17 +6,45 @@
 # #################################################################
 
 # -----------------------------------------------------------------
-# Capture arguments
+# Output help info
 # -----------------------------------------------------------------
-root_remote=$1;
-dir_remote=$2;
-host_name=$3;
-host_user=$4;
-host_pass=$5;
-dir_script=$6;
+
+# output help if requested
+if [ $1 == 'help' ]; then
+    echo "zen put code ................. Put live_code.tar.gz code on remote";
+    exit;
+fi
 
 # -----------------------------------------------------------------
-# Put code
+# Perform doctor diagnosis
+# -----------------------------------------------------------------
+
+# if diagnostics were requested
+if [ $1 == 'doctor' ]; then
+    # source the configuration file
+    source $2;
+    # tell the user the check is taking place
+    big_echo "Checking $dir_remote Directory";
+
+    # copy ssh password
+    echo "$host_pass SSH password on clipboard";
+    echo "$host_pass" | pbcopy;
+
+    # if the remote directory exists
+    if [[ `ssh "$host_user"@"$host_name" test -d $root_remote$dir_remote && echo exists` ]]; then
+        # report success
+        alert_success "The $root_remote$dir_remote directory exists";
+    else
+        # report failure
+        alert_error "The $root_remote$dir_remote directory does not exist";
+    fi
+
+    exit;
+
+fi
+
+# -----------------------------------------------------------------
+# Put code on remote server
 # -----------------------------------------------------------------
 
 # copy ssh password to clipboard
