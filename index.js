efc3db2dd3e9d6ff1c8f99897110fbd5ccb86e78
tcmulder <tcmulder@gmail.com>
2014-04-26 10:55:41 -0600

    Try to create node package

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-require('gulp-shell');
-module.exports = function() {
-    console.log("zen-deploy script running");
-}
+// /* jshint node:true */
+
+// 'use strict';
+
+// var Stream = require('stream');
+
+// module.exports = function() {
+//     var stream = new Stream.Transform({ objectMode: true });
+//     stream._transform = function(file, unused, done) {
+//         // When null just pass through
+//         if (file.isNull()) {
+//             this.push(file);
+//             done();
+//             return;
+//         }
+
+//         if (file.isBuffer()) {
+//             process.stdout.write(file.contents);
+//         } else {
+//             file.contents = file.contents.pipe(new Stream.PassThrough());
+//             file.contents.pipe(process.stdout);
+//         }
+
+//         this.push(file);
+//         done();
+//     };
+//     return stream;
+// };
