db9fc39c3d875b4a0ecb47eb7f3121d8590da5d9
tcmulder <tomas@zenman.com>
2014-05-30 10:10:51 -0600

    Separate far and stage db grab

--- modules/far.sh
+++ modules/far.sh
@@ -12,7 +12,7 @@
 
 # output help if requested
 if [ $1 == 'help' ]; then
-    echo "zen get db stage ............. Download database dump from staging server";
+    echo "zen far ...................... FAR URLs from stage_db.sql to live_db.sql";
     exit;
 fi
 
@@ -25,27 +25,23 @@ if [ $1 == 'doctor' ]; then
     # source the configuration file
     source $2;
     # tell the user the check is taking place
-    big_echo "Checking stage \"$db_stage_name\" Database";
+    big_echo "Checking Database FAR";
 
-    # copy the ssh password
-    echo "$host_stage_pass SSH password on clipboard";
-    echo "$host_stage_pass" | pbcopy;
-
-    # connect to the remote database and list tables
-    ssh_return=`ssh "$host_stage_user"@"$host_stage_name" "mysql -h$db_stage_host -u$db_stage_user -p$db_stage_pass $db_stage_name -e 'SHOW TABLES'"`;
+    # connect to the local database and list tables
+    mysql_return="`mysql -hlocalhost -uroot -proot -e 'SHOW DATABASES'`";
 
     # if the list of tables is empty
-    if [ ${#ssh_return} == 0 ]; then
+    if [ ${#mysql_return} == 0 ]; then
         # report failure
-        alert_error 'Could not connect to database';
+        alert_error 'Could not connect to local mysql server';
     # if the tables exist
     else
         # store the array of return data
-        table_array=($ssh_return);
+        table_array=($mysql_return);
         # report number of tables found
-        echo "tables recorded:  ${#table_array[@]}";
+        echo "databases recorded:  ${#table_array[@]}";
         # report success
-        alert_success 'Connected to stage database'
+        alert_success 'Connected to local mysql server'
     fi
 
     exit;
@@ -59,7 +55,7 @@ fi
 source $1;
 
 # establish desired output filename
-file_dump="$dir_backup""stage_db.sql";
+file_dump="$dir_backup""live_db.sql";
 
 # if the dump file doesn't already exist
 if [ ! -f $file_dump ]; then
@@ -67,12 +63,22 @@ if [ ! -f $file_dump ]; then
     #  if the backup directory exists
     if [ -d $dir_for_dump ]; then
 
-        # copy ssh password to clipboard
-        echo "$host_stage_pass SSH password on clipboard";
-        echo "$host_stage_pass" | pbcopy;
+        # create temporary database
+        temp_db="zen_script_"$((RANDOM%9000+1000))
+        mysql -uroot -proot -e "CREATE DATABASE $temp_db;" || alert_exit "Unable to create the $temp_db database";
 
-        # download database dump
-        ssh "$host_stage_user"@"$host_stage_name" "mysqldump -h$db_stage_host -u$db_stage_user -p$db_stage_pass $db_stage_name" | pv -Wbt | cat - > $file_dump;
+        # grab stage db
+        mysql -uroot -proot $temp_db < "$dir_backup"stage_db.sql || alert_exit "Cannot import "$dir_backup"stage_db.sql";
+
+        # replace url
+        php /Applications/MAMP/htdocs/_far/srdb.cli.php -hlocalhost -uroot -proot -n"$temp_db" -s"$url_stage" -r"$url"
+
+        php /Applications/MAMP/htdocs/_far.php $temp_db root root localhost utf8 $url_stage $url || alert_exit 'Unable to conduct find and replace on database';
+
+        # dump the temporary database then drop it
+        mysqldump -uroot -proot $temp_db > "$dir_backup"live_db.sql || alert_exit "Unable to dump database into "$dir_backup"live_db.sql";
+        mysql -uroot -proot -e "DROP DATABASE $temp_db;"
+
 
         # if the dump file now exists
         if [ -f $file_dump ]; then
@@ -81,8 +87,11 @@ if [ ! -f $file_dump ]; then
             du -ch $file_dump | grep total;
             # report success
             alert_success "Database file dumped";
+        # if the dump file does not exist
         else
-            alert_exit "The database was not successful dumped"
+            # report error
+            alert_error "Database dump failed"
+
         fi
 
     else
