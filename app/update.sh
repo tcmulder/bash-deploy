89a6f1de6ecbe27ee05bf687c5a07b87876d8c52
tcmulder <tcmulder@gmail.com>
2014-06-27 22:42:55 -0600

    Add update script

--- app/update.sh
+++ app/update.sh
@@ -6,27 +6,40 @@
 # #################################################################
 
 # -----------------------------------------------------------------
-# Main zen deploy function
+# Update script
 # -----------------------------------------------------------------
 
-echo 'yet something else';
-if [[ $(git --git-dir="$dir_script"/.git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
-    echo 'something';
-else
-    echo 'something else';
-fi
-
-
-# git_stat=`git --git-dir="$dir_script"/.git status status -uno`;
-# echo $git_stat;
-
-
-
-# check if on right branch and if up to date
-
 # if the zen function hasn't been initialized yet
-# if [ "`type -t zen`" != 'function' ]; then
+if [ "`type -t zen`" != 'function' ]; then
 
+    # set up git command in appropriate directory
+    git="git --git-dir="$dir_script"/.git";
 
+    # if the working directory isn't clean
+    if [ "`$git status`" != *"working directory clean"* ]; then
+        # tell the user
+        echo 'The working directory is unclean, which could cause problems.'
+        alert_error "Unclean git directory $dir_script";
+    else
+        # determine which branch is in use
+        branch="`$git rev-parse --abbrev-ref HEAD`";
+        # get the local and remote sha
+        local_commit="`$git rev-parse $branch`"
+        remote_commit="`$git rev-parse origin/$branch`"
 
-# fi
+        # if the local sha and remote sha are not the same
+        if [ local_commit != remote_commit ]; then
+            # ask the user if they'd like to update
+            echo "zen-deploy is out of date.";
+            read -p "Would you like to update? [YES|no] " confirm;
+            confirm=${confirm:-yes};
+            # if the answer is yes
+            if [ "$confirm" == 'yes' ]; then
+                # pull down changes
+                $git pull origin/$branch;
+            fi
+
+        fi
+    fi
+
+fi
