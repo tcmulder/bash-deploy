93170e808516ae142058a726015f1499f3dc5906
tcmulder <tcmulder@gmail.com>
2014-12-28 16:54:26 -0700

    Remove extra update check

--- zen-deploy.sh
+++ zen-deploy.sh
@@ -15,9 +15,6 @@
 # establish common functions
 source "$dir_script"/app/common_functions.sh;
 
-# check for updates
-source "$dir_script"/app/update.sh;
-
 # main zen script
 source "$dir_script"/app/zen.sh;
 
